29956382d4a51014699057076051ea0a2503651b
subhamproject <subhammandal01@gmail.com>
2018-05-16 18:31:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 export PATH="$PATH:/usr/local/bin"
-cd $(dirname $0)
-docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "runtests.sh" maven-app-build
+#cd $(dirname $0)
+docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "mvn clean test" maven-app-build
