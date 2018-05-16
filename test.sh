59c898ffe2826ea273aafd94084bde1b3387b21d
subhamproject <subhammandal01@gmail.com>
2018-05-16 18:23:59 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 export PATH="$PATH:/usr/local/bin:/$WORKSPACE"
-docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "runtests.sh" maven-app-build
+docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "../runtests.sh" maven-app-build
